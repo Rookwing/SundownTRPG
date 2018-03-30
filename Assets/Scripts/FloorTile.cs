983b336c342fad4777561d59b4131ee0a61cd742
RyDoug <rdougla7@email.cpcc.edu>
2018-03-29 22:31:18 -0400

    Begin working on floor selection and camera
    Added a script to be written later for the floor tiles.  Allowing selection of map objects on said floor tiles.
    
    Added a parent script for objects, like units, trees, etc.
    
    Manually modified camera start position to be centered.  This is not dynamic yet.

---
+++ Assets/Scripts/FloorTile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTile : MonoBehaviour {
+
+    //need typing for sprite selection
+
+    //need var to ref mapObject on this position
+
+    //
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MapObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObject : MonoBehaviour {
+
+    //parent to all objects on map
+
+    //needs basic interaction with floor, location etc.
+
+    //
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
